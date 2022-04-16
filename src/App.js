ebd8622efc63849b6874d1feb757e21ed5882d9f
jlamb2582 <lambj2582@gmail.com>
2022-04-15 20:13:59 -0400

    set up dictionary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <div className ="App-header">
+      <div className = "container">
+      <header className ="App-header">
       < img src ={logo} className = "App-logo img-fluid" alt = "logo" />
-        <p>
-          Hello There
-        </p>
-        </div>
+        </header>
+      <main>
+       <Dictionary />
+      </main>
+      <footer className ="App-footer">  
+       <small> Coded by Jessica Lamb 
+        </small>
+        </footer>
+      </div>
     </div>
   );
 }
