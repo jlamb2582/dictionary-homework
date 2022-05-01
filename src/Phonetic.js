8cd1e5155086c7611b8c66b68e9c9d455a24215f
jlamb2582 <lambj2582@gmail.com>
2022-05-01 13:59:14 -0400

    added photos css and js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic (props) {
 return (
 <div className ="Phonetic">
-<a href ={props.phonetic.audio} target ="blank" className="phonetic-link">
+<a href ={props.phonetic.audio} target ="blank"  rel="noreferrer" className="phonetic-link">
 Listen
 </a>
 <span className="text">
